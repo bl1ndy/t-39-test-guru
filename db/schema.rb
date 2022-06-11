2c9a9e352761508d85f9a2558b9c6efeb546d11f
bl1ndy <ayoblindstone@gmail.com>
2022-06-12 01:22:59 +0300

    feat: edit Test and Question
    Test: add default value to test level.
    Question: add attribute 'correct' with default value.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_215815) do
+ActiveRecord::Schema.define(version: 2022_06_11_221748) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -28,11 +28,12 @@ ActiveRecord::Schema.define(version: 2022_06_11_215815) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct", default: false
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
