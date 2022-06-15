0579a36f98922d7a6e025cb3763269adacb70cc4
bl1ndy <ayoblindstone@gmail.com>
2022-06-15 12:25:35 +0300

    feat: add class method to return tests titles by specified category

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,4 +4,11 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions, dependent: :destroy
   has_and_belongs_to_many :users # rubocop:disable Rails/HasAndBelongsToMany
+
+  class << self
+    def by_category(category)
+      category_id = Category.find_by(title: category).id
+      where(category_id:).order(title: :desc).pluck(:title)
+    end
+  end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class TestGuru::Application
-  VERSION = '0.10.10'
+  VERSION = '0.11.10'
 end
