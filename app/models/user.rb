aef6924c55fbc68d828beefbc19e9781bd69c473
bl1ndy <ayoblindstone@gmail.com>
2022-06-15 12:24:20 +0300

    feat: add instance method to return user tests by specified level value

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@
 
 class User < ApplicationRecord
   has_and_belongs_to_many :tests # rubocop:disable Rails/HasAndBelongsToMany
+
+  def tests_by_level(level)
+    tests.where(level:)
+  end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class TestGuru::Application
-  VERSION = '0.9.10'
+  VERSION = '0.10.10'
 end
