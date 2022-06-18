28e02b8495281352b437dbd172585ca912e3bb1e
bl1ndy <ayoblindstone@gmail.com>
2022-06-18 13:29:58 +0300

    feat: add answers count validation for Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,11 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
+  validate :validate_answers
+
+  private
+
+  def validate_answers
+    errors.add(:answers, 'are too much') if answers.count > 4
+  end
 end
