21a6464d96883562253968b948820878f66a8c47
bl1ndy <ayoblindstone@gmail.com>
2022-06-18 21:41:43 +0300

    fix: move answers count validation to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,18 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  ANSWERS_LIMIT_FOR_QUESTION = 4
+
   validates :body, presence: true
+  validate :validate_count
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_count
+    return unless question.answers.count == ANSWERS_LIMIT_FOR_QUESTION
+
+    errors.add(:answers, 'are too much for this question')
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,11 +5,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  validate :validate_answers
-
-  private
-
-  def validate_answers
-    errors.add(:answers, 'are too much') if answers.count > 4
-  end
 end
