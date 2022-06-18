c848941a38fc5df8377e1b35c454be110f9de3e7
bl1ndy <ayoblindstone@gmail.com>
2022-06-18 13:03:53 +0300

    feat: add Test validations
    Add title presence validation.
    Add level numericality validation.
    Add uniqueness validation for title-level pair

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,10 @@ class Test < ApplicationRecord
   has_many :test_passages, dependent: nil
   has_many :users, through: :test_passages
 
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+  validates :title, uniqueness: { scope: :level }
+
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class TestGuru::Application
-  VERSION = '0.18.17'
+  VERSION = '0.19.17'
 end
