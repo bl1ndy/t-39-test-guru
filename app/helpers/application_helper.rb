41c5eeb3b53609dc1d11936ef5375f050a2658a1
bl1ndy <ayoblindstone@gmail.com>
2022-06-24 11:14:35 +0300

    feat: add user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener'
   end
+
+  def flash_message
+    flash.map do |message_type, message|
+      content_tag(:div, message, class: "alert alert-#{message_type}")
+    end.join
+  end
 end
