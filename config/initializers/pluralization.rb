65f24e9c2ef48a1771d69f8e981b70621ace1537
bl1ndy <ayoblindstone@gmail.com>
2022-06-28 12:46:41 +0300

    feat: add 'ru' pluralization config

---
+++ config/initializers/pluralization.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'i18n/backend/pluralization'
+
+I18n::Backend::Simple.include I18n::Backend::Pluralization
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class TestGuru::Application
-  VERSION = '0.49.32'
+  VERSION = '0.50.32'
 end
