3402bf3e90990cd7582fb6d1770f28238346ec5c
bl1ndy <ayoblindstone@gmail.com>
2022-06-29 01:14:49 +0300

    feat: add localization for TestPassage
    Add localization for TestsMailer.
    Add localization for greeting in main menu.

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -5,6 +5,6 @@ class TestsMailer < ApplicationMailer
     @user = test_passage.user
     @test = test_passage.test
 
-    mail to: @user.email, subject: "You just completed the #{@test.title} Test!"
+    mail to: @user.email
   end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class TestGuru::Application
-  VERSION = '0.54.33'
+  VERSION = '0.55.33'
 end
