fe4d571533ae66540fc3adeaa7c4810a4f50e001
bl1ndy <ayoblindstone@gmail.com>
2022-07-09 00:49:00 +0300

    fix: fix test passage bugs
    Hide tests without questions from user.
    Add destroy dependence from test to test passages.
    Consider current question incorrect if no answer is selected.

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
-  has_many :test_passages, dependent: nil
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
 
   validates :title, presence: true
@@ -21,6 +21,10 @@ class Test < ApplicationRecord
       by_category(category)
         .order(title: :desc)
         .pluck(:title)
+    end
+
+    def with_questions
+      all.reject { |test| test.questions.count.zero? }
     end
   end
 end
