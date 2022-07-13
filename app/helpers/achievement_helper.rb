49605235a47be347e72fc947b394d2dc753a8315
bl1ndy <ayoblindstone@gmail.com>
2022-07-13 23:20:06 +0300

    fix: refactor Achievement service
    Edit achievement description helper.
    Edit locales.

--- app/helpers/achievement_helper.rb
+++ app/helpers/achievement_helper.rb
@@ -2,6 +2,6 @@
 
 module AchievementHelper
   def badge_description(badge)
-    t("achievements.helpers.descriptions.#{badge.rule.to_sym}")
+    t("achievements.helpers.descriptions.#{badge.rule.to_sym}", sub_rule: badge.sub_rule&.capitalize)
   end
 end
--- app/services/achievement_service.rb
+++ app/services/achievement_service.rb
@@ -1,61 +1,47 @@
 # frozen_string_literal: true
 
 class AchievementService
-  RULES_METHODS = {
-    no_rule: :no_rule,
-    first_attempt_success: :first_attempt_success,
-    all_backend: :all_by_category,
-    all_frontend: :all_by_category,
-    all_easy: :all_by_difficulty,
-    all_medium: :all_by_difficulty,
-    all_hard: :all_by_difficulty
-  }.freeze
-
   def initialize(test_passage)
     @test_passage = test_passage
     @user = @test_passage.user
     @test = @test_passage.test
     @category = @test.category.title
-    @level = @test.level
-    @achieved_badges = []
   end
 
   def call
     return if already_passed?
 
     Badge.all.each do |badge|
-      @achieved_badges << method(RULES_METHODS[badge.rule.to_sym]).call
+      @user.badges << badge if send(badge.rule, badge.sub_rule)
     end
-
-    reward(@achieved_badges.uniq)
   end
 
   private
 
-  def no_rule; end
+  def no_rule(_sub_rule); end
 
-  def first_attempt_success
-    return if @user.test_passages.where(test: @test).count > 1
-
-    Badge.find_by(rule: :first_attempt_success)
+  def first_attempt_success(_sub_rule)
+    @user.test_passages.where(test: @test).count == 1
   end
 
-  def all_by_category
+  def all_by_category(sub_rule)
+    return if @category != sub_rule.downcase.capitalize
+
     tests = Test.published.by_category(@category)
     passed_tests = passed_tests(tests)
-
-    Badge.find_by(rule: "all_#{@category.downcase}".to_sym) if passed_tests.count == tests.count
+    passed_tests.count == tests.count
   end
 
-  def all_by_difficulty
+  def all_by_difficulty(sub_rule)
+    return if difficulty != sub_rule.downcase.to_sym
+
     tests = Test.published.send(difficulty)
     passed_tests = passed_tests(tests)
-
-    Badge.find_by(rule: "all_#{difficulty.downcase}".to_sym) if passed_tests.count == tests.count
+    passed_tests.count == tests.count
   end
 
   def difficulty
-    case @level
+    case @test.level
     when 0..1 then :easy
     when 2..4 then :medium
     when 5..Float::INFINITY then :hard
@@ -64,12 +50,6 @@ class AchievementService
 
   def already_passed?
     @user.test_passages.passed.where(test: @test).count > 1
-  end
-
-  def reward(badges)
-    badges.each do |badge|
-      @user.achievements.create(badge:)
-    end
   end
 
   def passed_tests(tests)
